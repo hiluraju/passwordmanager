51facd1b734603677300df87f326655b2332d59a
Your Name <you@example.com>
2020-06-01 11:05:05 +0300

    404 page updated

--- application/views/error404.php
+++ application/views/error404.php
@@ -1,52 +1,47 @@
-<!doctype html>
-<html>
- <head>
-   <title>404 Page Not Found</title>
-   <style>
-   body{
-     width: 99%;
-     height: 100%;
-     background-color: mediumturquoise;
-     color: white;
-     font-family: sans-serif;
-   }
-   div {
-     position: absolute;
-     width: 400px;
-     height: 300px;
-     z-index: 15;
-     top: 45%;
-     left: 50%;
-     margin: -100px 0 0 -200px;
-     text-align: center;
-   }
-   h1,h2{
-     text-align: center;
-   }
-   h1{
-     font-size: 60px;
-     margin-bottom: 10px;
-     border-bottom: 1px solid white;
-     padding-bottom: 10px;
-   }
-   h2{
-     margin-bottom: 40px;
-   }
-   a{
-     margin-top:10px;
-     text-decoration: none;
-     padding: 10px 25px;
-     background-color: ghostwhite;
-     color: black;
-     margin-top: 20px;
-   }
-   </style>
- </head>
- <body>
-   <div>
-     <h1>404</h1>
-     <h2>Page not found</h2>
-     <a href='<?= base_url(); ?>' >Back to Homepage</a>
-   </div>
- </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>404 Page</title>
+
+    <!-- Custom fonts for this template-->
+  <link href="<?php echo base_url();?>lib/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+  <!-- Custom styles for this template-->
+  <link href="<?php echo base_url();?>lib/css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>lib/css/style.css" rel="stylesheet">
+</head>
+
+<body class="bg-gradient-danger">
+  <div class="container">
+      <div class="text-center mt20">
+        <h2 class="white wei">404</h2>
+        <hr class="backgroundwhite wei">
+        <br>
+        <h3 class="white wei"> Page not found </h3>
+        <br>
+        <a href="<?= base_url(); ?>">
+        <button class="btn btn-primary">Back to Homepage</button>
+        </a>
+      </div>
+  </div>
+
+
+ <!-- Bootstrap core JavaScript-->
+  <script src="<?php echo base_url();?>lib/jquery/jquery.min.js"></script>
+  <script src="<?php echo base_url();?>lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="<?php echo base_url();?>lib/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="<?php echo base_url();?>lib/js/sb-admin-2.min.js"></script>
+
+</body>
+
 </html>
